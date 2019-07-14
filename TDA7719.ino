27b97d00cb75d9e911dc2eb151b5b9128945e4f7
Alexander Liman <liman324@yandex.ru>
2019-07-14 11:35:45 +0600

    Update TDA7719.ino

--- TDA7719.ino
+++ TDA7719.ino
@@ -12,25 +12,25 @@ void loop(){
 }
 
 void audio(){
-tda.setInput(2,1,1,7); 
-tda.setInput_2(2,1,1,1,1,1);  
-tda.setMix_source(7,0); 
-tda.setMix_cont(1,1,1,1,1,1,1,1);
-tda.setMute(1,0,0,1,1,1,1); 
-tda.setSoft_1(1,1,1,1,1,1,1,1);
-tda.setSoft_2(1,1,1,1,0,3); 
-tda.setLoudness(0,0,1,1);  
-tda.setVol(0,1,1);
-tda.setTreble(0,3,1);
-tda.setMiddle(0,3,1); 
-tda.setBass(0,3,1); 
-tda.setSMB(0,1,0,0,1); 
-tda.setVol_LF(0,1); 
-tda.setVol_RF(0,1);
-tda.setVol_LR(0,1);
-tda.setVol_RR(0,1);
-tda.setVol_SUB_L(0,1); 
-tda.setVol_SUB_R(0,1);
-tda.setTest1(0,14,1,1);
-tda.setTest2(0,1,1,3);
+tda.setInput(2,1,1,7); // 0...7, 0...1, 0...1, 0...7
+tda.setInput_2(2,1,1,1,1,1); // 0...7, 0...1, 0...1, 0...1, 0...1, 0...1 
+tda.setMix_source(7,0); // 0...7, 0...-31
+tda.setMix_cont(1,1,1,1,1,1,1,1); // all 0...1
+tda.setMute(1,0,0,1,1,1,1); // 0...1, 0...1, 0...3, 0...1, 0...1, 0...1, 0...1
+tda.setSoft_1(1,1,1,1,1,1,1,1); //all 0...1
+tda.setSoft_2(1,1,1,1,0,3); // 0...1, 0...1, 0...1, 0...1, 0...2, 0...3
+tda.setLoudness(0,0,1,1); // 0...-15, 0...3, 0...1, 0...1 
+tda.setVol(0,1,1); // -15...+15, 0...1, 0...1
+tda.setTreble(0,3,1); // -15...+15, 0...3, 0...1
+tda.setMiddle(0,3,1); // -15...+15, 0...3, 0...1
+tda.setBass(0,3,1); // -15...+15, 0...3, 0...1
+tda.setSMB(0,1,0,0,1); // 0...3, 0...1, 0...3, 0...3, 0...1
+tda.setVol_LF(0,1); // 0...-79, 0...1
+tda.setVol_RF(0,1); // 0...-79, 0...1
+tda.setVol_LR(0,1); // 0...-79, 0...1
+tda.setVol_RR(0,1); // 0...-79, 0...1
+tda.setVol_SUB_L(0,1); // 0...-79, 0...1
+tda.setVol_SUB_R(0,1); // 0...-79, 0...1
+tda.setTest1(0,14,1,1); // 0...1, 0...15, 0...1, 0...1
+tda.setTest2(0,1,1,3); // 0...1, 0...1, 0...1, 0...3
 }
